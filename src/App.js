f903f50ae17efb8aa7958d32109a312c40887e32
SoloTufi <mat_990011@mail.ru>
2024-02-20 01:45:37 +0700

    все в пачке
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/App.js
+++ src/App.js
@@ -1,54 +1,106 @@
-import Card from "./components/Card";
-import Drawer from "./components/drawer";
-import Header from "./components/header";
+import React from 'react';
 
-const cardData = [
-  {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 12999,
-    imageUrl: "/img/sneakers/1.jpg"
-  },
-  {
-    title: "Мужские Кроссовки Nike Air Max 270",
-    price: 15632,
-    imageUrl: "/img/sneakers/2.jpg"
-  },
-  {
-    title: "Мужские Кроссовки Nike Blazer Mid Suede",
-    price: 12999,
-    imageUrl: "/img/sneakers/3.jpg"
-  },
-  {
-    title: "Мужские Кроссовки Nike Air Max 270",
-    price: 15632,
-    imageUrl: "/img/sneakers/4.jpg"
-  }
-]
+import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import { Drawer } from "./components/drawer";
+import { Header } from "./components/header";
+import { Home } from './pages/Home';
+import { Favorites } from './pages/Favorites';
 
 function App() {
+  //items и cartItems - хранят массивы объектов с данными о товарах и добавленных в корзину товарах соответственно
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+
+  const [favorites, setFavorites] = React.useState([]);
+
+  //searchValue - хранит строку с текущим значением поиска
+  const [searchValue, setSearchValue] = React.useState('');
+  //cartOpened - хранит булево значение, отображает открыта ли корзина
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  //Hook useEffect - выполняет асинхронный запрос на сервер для получения данных о товарах с помощью метода fetch. 
+  //После получения данных, они преобразуются в формат JSON и установятся в состояние items
+  React.useEffect(() => {
+    //res - ответ сервера
+    axios.get('https://87cbc8bfb5e8e5dc.mokky.dev/items').then((res) => {
+      setItems(res.data);
+      console.log(res.data);
+    })
+    axios.get('https://87cbc8bfb5e8e5dc.mokky.dev/cart').then((res) => {
+      setCartItems(res.data);
+    })
+    axios.get('https://87cbc8bfb5e8e5dc.mokky.dev/favorites').then((res) => {
+      setFavorites(res.data);
+      console.log(res.data);
+    })
+  }, []);
+
+  //onAddToCart - добавляет объект товара в массив cartItems с помощью функции setCartItems, используя оператор расширения массива
+  const onAddToCart = (obj) => {
+    axios.post('https://87cbc8bfb5e8e5dc.mokky.dev/cart', obj);
+    setCartItems((prev) => [...prev, obj]);
+    console.log(obj);
+  }
+
+  //onRemoveItem - удаляет объект из массива cartItems используя фильтрацию по id
+  const onRemoveItem = (id) => {
+    axios.delete(`https://87cbc8bfb5e8e5dc.mokky.dev/cart/${id}`);
+    setCartItems((prev) => prev.filter((item) => item.id !== id));
+  }
+
+  const onAddToFavorite = (obj) => {
+    if (favorites.find(obj => obj.id === obj.id)) {
+      axios.delete(`https://87cbc8bfb5e8e5dc.mokky.dev/favorites/${obj.id}`);
+      setFavorites((prev) => prev.filter((item) => item.id !== obj.id));
+    } else {
+      axios.post('https://87cbc8bfb5e8e5dc.mokky.dev/favorites', obj);
+      setFavorites((prev) => [...prev, obj]);
+    }
+
+  }
+
+
+  //onChangeSearchInput - обрабатывает событие изменения значения инпута поиска и выводит его в функцию setSearchValue
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+  //onClearSearchInput - очищает поле инпута, передавая в setSearchValue пустую строку
+  const onClearSearchInput = () => {
+    setSearchValue('');
+  }
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
-      <div className="content p-40">
-        <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
-          <div className="search-block d-flex">
-            <img src="/img/searchIcon.svg" alt="Search" />
-            <input placeholder="Поиск..." />
-          </div>
-        </div>
-        <div className="d-flex justify-between">
-          {cardData.map((obj) => (
-            <Card
-              title={obj.title}
-              price={obj.price}
-              imageUrl={obj.imageUrl} />
-          ))}
-        </div>
-      </div>
+
+      {/*Drawer - компонент для отображения корзины, который будет отображаться только при условии setCartOpened*/}
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
+      {/*Header - компонент для отображения шапки сайта, при клике на который будет открываться корзина*/}
+      <Header onClickCart={() => setCartOpened(true)} />
+
+      <Routes>
+        <Route path={"/"}
+          element={
+            <Home
+              items={items}
+              searchValue={searchValue}
+              onAddToCart={onAddToCart}
+              setSearchValue={setSearchValue}
+              onAddToFavorite={onAddToFavorite}
+              onClearSearchInput={onClearSearchInput}
+              onChangeSearchInput={onChangeSearchInput}
+            />
+          }>
+        </Route>
+        <Route path={"/favorites"}
+          element={<Favorites items={favorites} onAddToFavorite={onAddToFavorite} />}>
+        </Route>
+      </Routes>
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/drawer.jsx
+++ src/components/drawer.jsx
@@ -1,74 +1,78 @@
-function Drawer(props) {
+export const Drawer = ({ onClose, onRemove, items = [] }) => {
   return (
     <div className="overlay">
-      <div className="drawer d-flex flex-column">
-        <h2 className="mb-30 d-flex justify-between">
+      <div className="drawer">
+        <h2 className="d-flex justify-between mb-30">
           Корзина
           <img
-            src="/img/btn-remove.svg"
-            alt="Remove"
+            onClick={onClose}
             className="cu-p"
-            onClick={props.onClose}
+            src="/img/btn-remove.svg"
+            alt="Close"
           />
         </h2>
 
-        <div className="items flex">
-          <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
-              className="cartItemImg"
-            ></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img src="/img/btn-remove.svg" alt="Remove" className="removeBtn" />
-          </div>
-          <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/2.jpg)" }}
-              className="cartItemImg"
-            ></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img src="/img/btn-remove.svg" alt="Remove" className="removeBtn" />
-          </div>
-          <div className="cartItem d-flex align-center mb-20">
-            <div
-              style={{ backgroundImage: "url(/img/sneakers/3.jpg)" }}
-              className="cartItemImg"
-            ></div>
-            <div className="mr-20 flex">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img src="/img/btn-remove.svg" alt="Remove" className="removeBtn" />
-          </div>
-        </div>
+        {items.length > 0 ? (
+          <div>
+            <div className="items">
+              {items.map((obj) => (
+                <div className="cartItem d-flex align-center mb-20">
+                  <div
+                    style={{ backgroundImage: `url(${obj.imageUrl})` }}
+                    className="cartItemImg"
+                  ></div>
 
-        <div className="cartTotalBlock">
-          <ul>
-            <li>
-              <span>Итого:</span>
-              <div></div>
-              <b>21498 руб.</b>
-            </li>
-            <li>
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб.</b>
-            </li>
-          </ul>
-          <button className="greenButton">
-            Оформить заказ
-            <img src="/img/arrow.svg" alt="Arrow" width={13} height={13} />
-          </button>
-        </div>
+                  <div className="mr-20 flex">
+                    <p className="mb-5">{obj.title}</p>
+                    <b>{obj.price} руб.</b>
+                  </div>
+                  <img
+                    onClick={() => onRemove(obj.id)}
+                    className="removeBtn"
+                    src="/img/btn-remove.svg"
+                    alt="Remove"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="cartTotalBlock">
+              <ul>
+                <li>
+                  <span>Итого:</span>
+                  <div></div>
+                  <b>21 498 руб. </b>
+                </li>
+                <li>
+                  <span>Налог 5%:</span>
+                  <div></div>
+                  <b>1074 руб. </b>
+                </li>
+              </ul>
+              <button className="greenButton">
+                Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className="cartEmpty d-flex align-center justify-center flex-column flex">
+            <img
+              className="mb-20"
+              width="120px"
+              height="120px"
+              src="/img/cart-empty.png"
+              alt="Empty"
+            />
+            <h2>Корзина пустая</h2>
+            <p className="opacity-6">
+              Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.
+            </p>
+            <button onClick={onClose} className="greenButton">
+              <img src="/img/arrow.svg" alt="Arrow" />
+              Вернуться назад
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
-}
-
-export default Drawer;
+};
